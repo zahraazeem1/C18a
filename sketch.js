ba7501978b34a16d1d3f63f6775856337fccf75a
zahraazeem1 <114787158+zahraazeem1@users.noreply.github.com>
2022-12-23 18:41:53 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var s1
+
+function setup() 
+{
+  createCanvas(400, 400);
+  s1 = new Student("john",12,6);
+  s1.display()
+
+}
+function draw() 
+{
+  background(51);
+ 
+}
+
+
---
+++ student.js
@@ -0,0 +1,21 @@
+class Student
+  {
+    constructor(name,age,grade)
+    {
+      this.name = name;
+      this.grade = grade;
+      this.age = age;
+      
+    }
+    
+    display()
+    {
+      console.log(this.name);
+      console.log(this.age);
+      console.log(this.grade);
+    }
+    
+  
+  }
+
+  
